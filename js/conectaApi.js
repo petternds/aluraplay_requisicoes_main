6ff5fddf859f0925d7af9ac47989bf96e177fd8a
Peter <petternds@gmail.com>
2024-08-13 07:13:46 -0300

    projeto finalizado

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -9,7 +9,7 @@ async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
-            "content-type": "aplication/json"
+            "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
@@ -19,11 +19,23 @@ async function criaVideo(titulo, descricao, url, imagem) {
         })
     });
 
-    const conexaoConvertida = await conexao.json();
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
+
+    const conexaoConvertida = conexao.json();
     return conexaoConvertida;
 };
 
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:300/videos?q=${termoDeBusca}`);
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida
+}
+
 export const conectaApi = {
     listaVideos,
-    criaVideo
+    criaVideo,
+    buscaVideo
 };
--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,7 +1,6 @@
 import {
     conectaApi
 } from "./conectaApi.js";
-
 const formulario = document.querySelector("[data-formulario]");
 
 async function criarVideo(evento) {
@@ -11,10 +10,12 @@ async function criarVideo(evento) {
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
-
-    await conectaApi.criaVideo(titulo, descricao, url, imagem);
-
-    window.location.href = "../pages/envio-concluido.html";
-};
+    try {
+        await conectaApi.criaVideo(titulo, descricao, url, imagem);
+        window.location.href = "../pages/envio-concluido.html";
+    } catch (e) {
+        alert(e);
+    }
+}
 
 formulario.addEventListener("submit", evento => criarVideo(evento))
